c691e5c8b6f9cf64260332946f7ede0f00001464
Spencer Alderman <spencer@1rogue.com>
2014-06-21 20:35:38 -0400

    Add yml's

--- src/main/java/com/codelanx/enderblack/EnderBlack.java
+++ src/main/java/com/codelanx/enderblack/EnderBlack.java
@@ -24,6 +24,7 @@ import com.codelanx.codelanxlib.implementers.Configurable;
 import com.codelanx.codelanxlib.implementers.Listening;
 import com.codelanx.codelanxlib.implementers.Reloadable;
 import com.codelanx.codelanxlib.listener.ListenerManager;
+import com.codelanx.codelanxlib.util.DebugUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -40,6 +41,7 @@ public class EnderBlack extends JavaPlugin implements Configurable, Listening, R
 
     @Override
     public void onEnable() {
+        DebugUtil.toggleOutput(false);
         this.config = new ConfigurationLoader(this, ConfigValue.class);
         this.listener = new ListenerManager<>(this);
         this.listener.registerListeners(new EnderChestListener(this));
